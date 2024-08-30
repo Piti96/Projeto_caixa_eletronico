40df16721a89e2f1860d8476d63dc8461a8e2e55
Erivelton De Oliveira <107161176+Piti96@users.noreply.github.com>
2024-08-29 22:18:32 -0300

    Add files via upload

--- programa_caixa_banco.py
+++ programa_caixa_banco.py
@@ -1,13 +1,80 @@
-print(
-"""
+menu  = """
 __________MENU________
-1 - Sacar
-2 - Depositar
-3 - Extrato
-4 - Sair
+[1] - Depositar
+[2] - Sacar
+[3] - Extrato
+[4] - Sair
 _______________________
 
 """
-)
+saldo = 100
+limite = 500
+extrato = " "
+numero_saque = 0
+LIMITE_SAQUES = 3
 
-operacao = int(input("Digite a opcão desejada: "))
+
+while True:
+    print(menu)
+    
+    opcao = int(input("Ecolha a opção desejada: "))
+    
+    if opcao == 1:
+       valor = float(input("Digite o valor do deposito: "))
+       if valor > 0:
+           saldo += valor
+           extrato += f"Deposito: R$ {valor}\n"
+           
+       else:
+           print("A operação falhou! Tente novamente.")
+           
+           
+           
+    elif opcao == 2:
+           valor = float(input(" Informe i valor que deseja sacar: "))
+           
+           saldo_insuficiente = valor > saldo
+           
+           limite_insuficiente = valor > limite
+           
+           excedeu_saques = numero_saque >= LIMITE_SAQUES
+           
+           if valor > 0:
+              saldo -= valor
+              extrato += f" Saque: R$ {valor}\n"
+              numero_saque += 1
+            
+           
+           
+           elif saldo_insuficiente:
+               print ("Saldo insuficiente para realizar a operação!")
+           
+           elif limite_insufuciente:
+                  print ("Você não possui limite disponivel para realizar essa operação!")
+            
+           elif excedeu_saques:
+                  print ("A operação não pode ser realizada, pois você atingiu o limite diário de saques!")
+               
+             
+           
+           else:
+               print("A operacao falhou")
+               
+               
+    elif opcao == 3:
+           print ("\n__________EXTRATO__________")
+           print (" Não foram realizadas movimentações no período."   if not extrato   else extrato)
+           print (f"\n Saldo: R$ {saldo}")
+           print ("________________________________")
+               
+               
+               
+    elif opcao ==4:
+               print("Saindo, obrigado por utilizar nossos serviços!")
+               break
+               
+               
+    else:
+               print("Operção falhou. tente novamente!")
+               
+           
